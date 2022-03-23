f0e49ee18654ff444f0f9526d53dc5fffefc781a
Murad Alizade <muradsepic@gmail.com>
2022-03-23 14:10:01 +0400

    Extentions

---
+++ Extensions/Extensions/Extensions/Extensions.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions.Extensions
+{
+	public static class Extensions
+	{
+		/// <summary>
+		/// Capitalize the text
+		/// </summary>
+		/// <param text: ="text"></param>
+		/// <returns></returns>
+		public static string Capitalize(this string text)
+		{
+			char firstLetter = Char.ToUpper(text[0]);
+			StringBuilder stringBuilder = new StringBuilder(text);
+			stringBuilder[0] = firstLetter;
+			return stringBuilder.ToString();
+		}
+		/// <summary>
+		/// Reverce the text
+		/// </summary>
+		/// <param text: ="text"></param>
+		/// <returns></returns>
+		public static string Reverse(this string text)
+		{
+			char[] reversible = text.ToCharArray();
+			string reversedText = "";
+			for (int i = reversible.Length - 1; i >= 0; i--)
+			{
+				reversedText += reversible[i];
+			}
+			return reversedText;
+		}
+		/// <summary>
+		/// Gets letter from your index
+		/// </summary>
+		/// <param text: ="text"></param>
+		/// <param index: ="index"></param>
+		/// <returns></returns>
+		public static string GetLetter(this string text, int index)
+        {
+			char[] letters = text.ToCharArray();
+			string letter = "";
+			return letter += letters[index];
+		}
+		/// <summary>
+		/// Adds element into your array
+		/// </summary>
+		/// <param array: ="stringArr"></param>
+		/// <param index: ="index"></param>
+		/// <param element: ="text"></param>
+		/// <returns></returns>
+		public static string[] Add(this string[] stringArr, int index, string text)
+        {
+			string[] added = new string[stringArr.Length + 1];
+            for (int i = 0; i < added.Length; i++)
+            {
+                if (i == added.Length - 1)
+                {
+					added[i] = text;
+					break;
+                }
+				added[i] = stringArr[i];
+			}
+			string temp = added[index];
+			added[index] = added[added.Length - 1];
+			added[added.Length - 1] = temp;
+			return added;
+		}
+		/// <summary>
+		/// Adds element into your array
+		/// </summary>
+		/// <param array: ="charArr"></param>
+		/// <param index: ="index"></param>
+		/// <param element: ="character"></param>
+		/// <returns></returns>
+		public static char[] Add(this char[] charArr, int index, char character)
+		{
+			char[] added = new char[charArr.Length + 1];
+			for (int i = 0; i < added.Length; i++)
+			{
+				if (i == added.Length - 1)
+				{
+					added[i] = character;
+					break;
+				}
+				added[i] = charArr[i];
+			}
+			char temp = added[index];
+			added[index] = added[added.Length - 1];
+			added[added.Length - 1] = temp;
+			return added;
+		}
+		/// <summary>
+		/// Adds element into your array
+		/// </summary>
+		/// <param array: ="intArr"></param>
+		/// <param index: ="index"></param>
+		/// <param element: ="number"></param>
+		/// <returns></returns>
+		public static int[] Add(this int[] intArr, int index, int number)
+		{
+			int[] added = new int[intArr.Length + 1];
+			for (int i = 0; i < added.Length; i++)
+			{
+				if (i == added.Length - 1)
+				{
+					added[i] = number;
+					break;
+				}
+				added[i] = intArr[i];
+			}
+			int temp = added[index];
+			added[index] = added[added.Length - 1];
+			added[added.Length - 1] = temp;
+			return added;
+		}
+	}
+}
---
+++ Extensions/Extensions/Program.cs
@@ -0,0 +1,52 @@
+﻿using Extensions.Extensions;
+using System;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "extensions";
+            string capitalizedText = text.Capitalize();
+            string revercedText = text.Reverse();
+            string letter = text.GetLetter(2);
+            string[] stringArr = { "Extensions", "Capitalize", "Reverse" };
+            string[] addedStringArr = stringArr.Add(1, text);
+            char[] charArr = { 'e', 'x', 't', 'e', 'n', 't', 'i', 'o', 'n', 's' };
+            char[] addedCharArr = charArr.Add(1, 's');
+            int[] intArr = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] addedIntArr = intArr.Add(2, 4);
+            Console.WriteLine("Before: " + text + "\nCapitalized: " + capitalizedText + "\nReverced: " + revercedText + "\nLetter: " + letter + "\nString array: ");
+            foreach (var item in stringArr)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine("\nAdded string array: ");
+            foreach (var item in addedStringArr)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine("\nChar array: ");
+            foreach (var item in charArr)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine("\nAdded char array: ");
+            foreach (var item in addedCharArr)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine("\nInt array: ");
+            foreach (var item in intArr)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine("\nAdded int array: ");
+            foreach (var item in addedIntArr)
+            {
+                Console.Write(item + ", ");
+            }
+        }
+    }
+}
